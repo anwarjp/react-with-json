cc2d448f69ab5797027d92d16283eab4d468d34f
AH Anwar <anwar.cseian@gmail.com>
2023-09-10 18:43:05 +0600

    code updated

--- src/User.jsx
+++ src/User.jsx
@@ -1,8 +1,9 @@
 export default function User({user}) {
+    const {name,email} = user; //object destructuring
     return (
         <div className="box">
-            <h2>User's name : {user.name}</h2>
-            <p>User's email : {user.email}</p>
+            <h2>User's name : {name}</h2>
+            <p>User's email : {email}</p>
         </div>
     )
 }
